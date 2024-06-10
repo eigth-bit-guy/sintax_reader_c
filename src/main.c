58776eca270317c9518ff57feb67c390ae831bc2
Samuel Henrique <samuel@voyager>
2024-06-10 00:53:22 -0300

    remove if

--- src/main.c
+++ src/main.c
@@ -26,11 +26,6 @@
 int main(int argc, char **argv)
 {
   int c; 
-  if(argc < 1){
-    fprintf(stderr, "error: parameter missing\n");
-    return -1;
-  }
-  
   FILE *file = fopen(argv[1], "r");
   while((c = fgetc(file)) != EOF){
     switch(c){
